7db9f83b1a33f9cc32a18423463f300c1f2d456e
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-08-13 23:45:05 +0900

    [Clean] Split the Webpack files

---
+++ config/webpack.common.js
@@ -0,0 +1,56 @@
+//@ts-check
+
+"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+/**@type {import('webpack').Configuration}*/
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, "../src/popup/index.tsx"),
+    eventPage: path.join(__dirname, "../src/eventPage.ts"),
+  },
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.join(__dirname, "../dist/js"),
+    filename: "[name].js",
+  },
+  externals: {
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vsceignore file
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // Creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // Translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
+};
